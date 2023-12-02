7dfb200973fe11ae5b312539a2965591b76f999e
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-12-02 15:37:54 +0100

    Extract common code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,6 +7,7 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
 import util.NumberResponse
+import java.math.BigInteger
 
 fun main(args: Array<String>) {
 // TODO: Add port parameter
@@ -16,25 +17,27 @@ fun main(args: Array<String>) {
                 call.respondText("""{ "response" : "Hello, world!!" }""", ContentType.Application.Json)
             }
             post("/day01") {
-                val input = call.receiveText()
-                val solution1 = day01.Day01.part1(input)
-                val solution2 = day01.Day01.part2(input)
-                val response = NumberResponse(solution1, solution2)
-                call.respondText(
-                    Json.encodeToString(NumberResponse.serializer(), response),
-                    ContentType.Application.Json
-                )
+                collectNumberSolutionWith(call, day01.Day01::part1, day01.Day01::part2)
             }
             post("/day02") {
-                val input = call.receiveText()
-                val solution1 = day02.Day02.part1(input)
-                val solution2 = day02.Day02.part2(input)
-                val response = NumberResponse(solution1.toBigInteger(), solution2)
-                call.respondText(
-                    Json.encodeToString(NumberResponse.serializer(), response),
-                    ContentType.Application.Json
-                )
+                collectNumberSolutionWith(call, { i -> day02.Day02.part1(i).toBigInteger() }, day02.Day02::part2)
             }
         }
     }.start(wait = true)
+}
+
+private suspend fun collectNumberSolutionWith(
+    call: ApplicationCall,
+    solution1: (String) -> BigInteger, solution2: (String) -> BigInteger
+
+): Unit {
+    val input = call.receiveText()
+    val response = NumberResponse(
+        solution1(input),
+        solution2(input)
+    )
+    return call.respondText(
+        Json.encodeToString(NumberResponse.serializer(), response),
+        ContentType.Application.Json
+    )
 }
