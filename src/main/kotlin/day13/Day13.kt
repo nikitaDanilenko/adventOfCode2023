86f298a43ec71a1b2c2d89a47acd633517d5145e
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-12-15 00:31:37 +0100

    Add solution for day 13, part 2

--- src/main/kotlin/day13/Day13.kt
+++ src/main/kotlin/day13/Day13.kt
@@ -11,9 +11,22 @@ object Day13 {
     }
 
     private fun solution1(patterns: List<Pattern>): BigInteger =
-        patterns.sumOf { findFirstReflection(it).toBigInteger() }
+        patterns.sumOf {
+            findFirstReflectionWith(
+                it,
+                ::findHorizontalReflection,
+                ::findVerticalReflection
+            ).toBigInteger()
+        }
 
-    private fun solution2(lines: List<Pattern>): BigInteger = BigInteger.ZERO
+    private fun solution2(patterns: List<Pattern>): BigInteger =
+        patterns.sumOf {
+            findFirstReflectionWith(
+                it,
+                ::findSmudgeHorizontalReflectionLine,
+                ::findSmudgeVerticalReflectionLine
+            ).toBigInteger()
+        }
 
     data class Position(
         val line: Int,
@@ -44,14 +57,19 @@ object Day13 {
             .map(::parsePattern)
     }
 
+    private fun createLineMap(
+        positions: Set<Position>
+    ): Map<Int, Set<Int>> =
+        positions.groupBy { it.line }.mapValues { it.component2().map { pos -> pos.column }.toSet() }
+
     private fun findHorizontalReflection(pattern: Pattern): Int? {
-        val lines = pattern.positions.groupBy { it.line }.mapValues { it.component2().map { pos -> pos.column } }
+        val lines = createLineMap(pattern.positions)
         val candidates =
             (0..pattern.height)
                 .zipWithNext()
                 .filter { (i1, i2) ->
-                    val line1 = lines.getOrDefault(i1, emptyList())
-                    val line2 = lines.getOrDefault(i2, emptyList())
+                    val line1 = lines.getOrDefault(i1, emptySet())
+                    val line2 = lines.getOrDefault(i2, emptySet())
                     line1 == line2
                 }
                 .map { it.first }
@@ -71,7 +89,7 @@ object Day13 {
 
     private fun reflectsAlongLine(
         line: Int,
-        lines: Map<Int, List<Int>>,
+        lines: Map<Int, Set<Int>>,
     ): Boolean {
         val counterLine = 2 * line + 1
         return (0..line).all { index ->
@@ -88,10 +106,46 @@ object Day13 {
         }
     }
 
-    private fun findFirstReflection(pattern: Pattern): Int {
+    private fun findFirstReflectionWith(
+        pattern: Pattern,
+        findHorizontalReflection: (Pattern) -> Int?,
+        findVerticalReflection: (Pattern) -> Int?,
+    ): Int {
         val horizontal = findHorizontalReflection(pattern)
         val vertical = findVerticalReflection(pattern)
         return (horizontal?.let { (1 + it) * 100 }) ?: (1 + vertical!!)
+    }
+
+    private fun <A> symmetricDifference(
+        set1: Set<A>,
+        set2: Set<A>,
+    ): Set<A> =
+        set1.minus(set2).union(
+            set2.minus(set1)
+        )
+
+    private fun findSmudgeVerticalReflectionLine(
+        pattern: Pattern
+    ): Int? = findSmudgeHorizontalReflectionLine(transposePattern(pattern))
+
+    private fun findSmudgeHorizontalReflectionLine(
+        pattern: Pattern
+    ): Int? {
+        val lines = createLineMap(pattern.positions)
+        return (0..pattern.height).flatMap { first ->
+            (first + 1..pattern.height).step(2).map { first to it }
+        }.mapNotNull { (first, second) ->
+            val line1 = lines.getOrDefault(first, emptySet())
+            val line2 = lines.getOrDefault(second, emptySet())
+            val symmetricDifference = symmetricDifference(line1, line2)
+            if (symmetricDifference.size == 1)
+                Position(line = first, column = symmetricDifference.first()) to first + (second - first - 1) / 2
+            else null
+        }.firstOrNull { (candidatePosition, reflectionLine) ->
+            val newPositions = symmetricDifference(pattern.positions, setOf(candidatePosition))
+            val newLines = createLineMap(newPositions)
+            reflectsAlongLine(reflectionLine, newLines)
+        }?.second
     }
 
 }
