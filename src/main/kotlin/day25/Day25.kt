13edd2bd3cb2d0d1b283a2ed1bfdd84a125a406e
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2024-01-19 20:34:55 +0100

    Add quasi-solution for day 25.1

--- src/main/kotlin/day25/Day25.kt
+++ src/main/kotlin/day25/Day25.kt
@@ -10,31 +10,60 @@ object Day25 {
     }
 
     private fun solution1(graph: Graph): BigInteger {
-        return BigInteger.ZERO
+        return heuristicMinCut(graph).toBigInteger()
     }
 
+    // There is no part 2.
     private fun solution2(graph: Graph): BigInteger {
         return BigInteger.ZERO
     }
 
     data class Graph(
-        val adjacency: Map<String, List<String>>
+        val adjacency: Map<String, Set<String>>
     ) {
         companion object {
             fun parse(input: String): Graph {
-                val adjacency = input.lines().flatMap { line ->
-                    val (from, tosString) = line.split(": ")
-                    tosString.split(" ").flatMap { listOf(it to from, from to it) }
-                }
+                val adjacency = input
+                    .lines()
+                    .flatMap { line ->
+                        val (from, tosString) = line.split(": ")
+                        tosString
+                            .split(" ")
+                            .flatMap { listOf(it to from, from to it) }
+                    }
                     .groupBy { it.first }
                     .mapValues {
                         it
                             .value
                             .map { pair -> pair.second }
-
+                            .toSet()
                     }
                 return Graph(adjacency)
             }
         }
+    }
+
+    // This is a heuristic presented on Reddit.
+    // The assumption is that the graph is sufficiently well-formed.
+    private fun heuristicMinCut(graph: Graph): Int {
+        val nodes = graph.adjacency.keys
+
+        fun outgoingEdges(node: String, partition: Set<String>): Int =
+            graph
+                .adjacency[node]!!
+                .minus(partition)
+                .size
+
+        tailrec fun iterate(partition: Set<String>): Int {
+            val allOutgoing = partition.sumOf { outgoingEdges(it, partition) }
+            if (allOutgoing == 3)
+                return partition.size * nodes.minus(partition).size
+            else {
+                val maxOutgoing = partition.maxBy { outgoingEdges(it, partition) }
+                return iterate(partition - maxOutgoing)
+            }
+        }
+
+        return iterate(nodes)
     }
 }
